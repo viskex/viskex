581cdd3c8e6b09f585f9102f78ce6f4284da9510
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-03-06 07:18:01 +0100

    Add tutorials conftest

---
+++ tutorials/conftest.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2023 by the viskex authors
+#
+# This file is part of viskex.
+#
+# SPDX-License-Identifier: MIT
+"""pytest configuration file for tutorials tests."""
+
+import nbvalx.pytest_hooks_notebooks
+import pytest
+
+pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
+pytest_sessionstart = nbvalx.pytest_hooks_notebooks.sessionstart
+pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
+pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
+pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
+
+
+def pytest_runtest_setup(item: pytest.File) -> None:
+    """Check backend availability."""
+    # Do the setup as in nbvalx
+    nbvalx.pytest_hooks_notebooks.runtest_setup(item)
+    # Get notebook name
+    notebook_name = item.parent.name
+    # Check backend availability depending on the item name
+    if notebook_name.endswith("dolfinx.ipynb"):
+        pytest.importorskip("dolfinx")
+    elif notebook_name.endswith("firedrake.ipynb"):
+        pytest.importorskip("firedrake")
+    else:
+        raise ValueError("Invalid notebook name " + notebook_name)
