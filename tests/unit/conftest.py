8da83fc74f86ea72fe0a7575bf3eb8542ce03f00
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-03-06 07:15:42 +0100

    Add unit tests conftest

---
+++ tests/unit/conftest.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2023 by the viskex authors
+#
+# This file is part of viskex.
+#
+# SPDX-License-Identifier: MIT
+"""pytest configuration file for unit tests."""
+
+import nbvalx.pytest_hooks_unit_tests
+
+pytest_runtest_setup = nbvalx.pytest_hooks_unit_tests.runtest_setup
+pytest_runtest_teardown = nbvalx.pytest_hooks_unit_tests.runtest_teardown
