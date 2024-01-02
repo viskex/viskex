158e400cd2f4a72c78aea6a46fa09ee34c73d3f8
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-02 09:07:11 +0100

    Add a mock test to restore working CI

---
+++ tests/unit/test_empty.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2023-2024 by the viskex authors
+#
+# This file is part of viskex.
+#
+# SPDX-License-Identifier: MIT
+"""Temporary mock test to allow pytest to run on at least a test."""
+
+def test_empty() -> None:
+    """Temporary mock test to allow pytest to run on at least a test."""
+    pass
