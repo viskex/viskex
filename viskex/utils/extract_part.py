caff566332ed2314de3f96ddfc5cb5d27c39ae42
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-02 09:18:38 +0100

    Enforce coverage of scalar types

--- viskex/utils/extract_part.py
+++ viskex/utils/extract_part.py
@@ -17,7 +17,7 @@ def extract_part(
     values: np.typing.NDArray[ScalarType], name: str, part: str
 ) -> typing.Tuple[np.typing.NDArray[np.float64], str]:
     """Extract real or complex part from an array, and update the name to reflect this."""
-    if np.issubdtype(ScalarType, np.complexfloating):  # pragma: no cover
+    if np.issubdtype(ScalarType, np.complexfloating):
         if part == "real":
             values = values.real
             name = "real(" + name + ")"
