2e39c33212ed718101a473f0415881c13eaaa03f
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-10 08:36:04 +0100

    Move notebooks tests back into tutorials after failed attempt to use them as tests

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -9,10 +9,19 @@ import nbvalx.pytest_hooks_notebooks
 import pytest
 
 pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
-pytest_sessionstart = nbvalx.pytest_hooks_notebooks.sessionstart
 pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
 pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
 pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
+
+
+def pytest_sessionstart(session: pytest.Session) -> None:
+    """Automatically mark common files as data to be linked in the work directory."""
+    # Add common files as data to be linked
+    link_data_in_work_dir = session.config.option.link_data_in_work_dir
+    assert len(link_data_in_work_dir) == 0
+    link_data_in_work_dir.append("**/common_*.py")
+    # Start session as in nbvalx
+    nbvalx.pytest_hooks_notebooks.sessionstart(session)
 
 
 def pytest_runtest_setup(item: nbvalx.pytest_hooks_notebooks.IPyNbFile) -> None:
