b6ecd1d9fb8bf880e032ba488ec4353c28be02c7
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-15 12:58:14 +0100

    Drop nbvalx hooks which have been removed upstream

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -10,8 +10,6 @@ import pytest
 
 pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
 pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
-pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
-pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
 
 
 def pytest_sessionstart(session: pytest.Session) -> None:
@@ -26,8 +24,6 @@ def pytest_sessionstart(session: pytest.Session) -> None:
 
 def pytest_runtest_setup(item: nbvalx.pytest_hooks_notebooks.IPyNbFile) -> None:
     """Check backend availability."""
-    # Do the setup as in nbvalx
-    nbvalx.pytest_hooks_notebooks.runtest_setup(item)
     # Get notebook name
     notebook_name = item.parent.name
     # Check backend availability depending on the item name
